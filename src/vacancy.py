c2b6dd5fa52e0cc785f756d564410fa4b4ab4438
Denis_L <denis88lyapin@gmail.com>
2023-06-14 23:12:50 +0300

    added sort_vacancy_payment, sort_vacancy_date and delete_vacancy in Vacancy to vacancy.py

--- src/vacancy.py
+++ src/vacancy.py
@@ -1,8 +1,8 @@
-
+from datetime import datetime
 class Vacancy:
 
-    __slots__ = ("platform", "title", "company", "url", "area", "address", "candidat",
-                 "vacancy_rich_text", "date_published", "payment")
+    __slots__ = ('id', "platform", "title", "company", "url", "area", "address", "candidat",
+                 "vacancy_rich_text", "date_published", "payment", "priority")
     """
     Класс для работы с вакансиями. Инициализируется по данным потомков класса JobPlatformAPI.
     Хранит список вакансий. Поддерживает методы сравнения и сортировки вакансий по зарплате и дате.
@@ -12,6 +12,7 @@ class Vacancy:
         try:
             if vacancy:
                 self.platform = vacancy.get('platform', 'нет данных')
+                self.id = vacancy.get("id")
                 self.title = vacancy.get('title', 'нет данных')
                 self.company = vacancy.get('company', 'нет данных')
                 self.url = vacancy.get('url', 'нет данных')
@@ -19,9 +20,16 @@ class Vacancy:
                 self.address = vacancy.get('address', 'нет данных')
                 self.candidat = vacancy.get('candidat', 'нет данных')
                 self.vacancy_rich_text = vacancy.get('vacancyRichText', 'нет данных')
-                self.date_published = vacancy.get('date_published', 'нет данных')
                 self.payment = vacancy.get('payment', 'нет данных')
+                date_str = vacancy.get('date_published')
+                if date_str:
+                    self.date_published = datetime.strptime(date_str, '%Y.%m.%d %H:%M:%S')
+                else:
+                    self.date_published = 0
+                if not
+                self.priority = False
                 Vacancy.all.append(self)
+
             else:
                 raise ValueError("Вакансия не найдена")
         except ValueError as error:
@@ -29,7 +37,8 @@ class Vacancy:
             print("Повторите запрос")
 
     def __str__(self):
-        return f'"Title: {self.title}\n' \
+        return f'id: {self.id}\n' \
+               f'Title: {self.title}\n' \
                f'Payment: {self.payment}\n' \
                f'Area: {self.area}\n' \
                f'URL: {self.url}'
@@ -65,49 +74,83 @@ class Vacancy:
             payment = payment_from
         return payment
 
-    def sort_vacancy_payment(self):
-        for vacancy in Vacancy.all:
-            pass
+    def sort_vacancy_payment(self) -> None:
+        """
+        Функция сортирует вакансии по зарплате.
+        """
+        Vacancy.all.sort(key=lambda vacancy: vacancy.get_payment())
 
+    def sort_vacancy_date(self):
+        """
+        Функция сортирует вакансии по зарплате.
+        """
+        Vacancy.all.sort(key=lambda vacancy: vacancy.date_published, reverse=True)
 
+    def delete_vacancy(self, id):
+        """
+        Функция удаляет вакансию из списка Vacancy.all на основе указанного идентификатора (id).
+        """
+        try:
+            if id.isdigit():
+                vacancy_found = False
+                filtered_vacancies = filter(lambda vacancy: vacancy.id == id, Vacancy.all)
+                for vacancy in filtered_vacancies:
+                    Vacancy.all.remove(vacancy)
+                    vacancy_found = True
 
-
-
-
+                if not vacancy_found:
+                    raise ValueError("Вакансия не найдена. Введите корректный идентификатор вакансии.")
+            else:
+                raise ValueError("Идентификатор вакансии должен состоять только из цифр.")
+        except ValueError as error:
+            print(str(error))
 
 
 if __name__ == '__main__':
-    data = {
-    "platform": "SuperJob",
-    "title": "Специалист по парсерам",
-    "company": "JOBCART.RU",
-    "url": "https://www.superjob.ru/vakansii/specialist-po-parseram-46449440.html",
-    "area": "Москва",
-    "address": "г Москва, ул Каланчевская, д 20",
-    "candidat": "Вакансия компании \"MARPLA\"\nМARketPLAce - Сервис анализа поисковых запросов на Wildberries.\nОбязанности:\n- Разработка и сопровождение парсера для сбора данных с маркетплейса Wildberries.\nТребования:\n- Положительный опыт работы в команде;\n- Выходить во внерабочее время (при необходимости), для устранения критических и аварийных багов парсера;\n- Высшее техническое образование;\n- Опыт высоконагруженного парсинга:\nмногопоточный парсинг через прокси в несколько тысяч потоков;\nот 100 млн запросов в сутки;\nот 0.5 млрд записей результатов в сутки;\n- Знание python, pandas;\n- Понимание принципов работы поисковых систем, обязательных требований к сайтам;\n- ООП, структуры данных, базовые алгоритмы и понимание их сложности, базовое знание HTML/CSS/JS, базовый Git.\nУсловия:\n- Удаленный формат работы;\n- Разнообразные интересные мотивирующие задачи;\n- Своевременная выплата заработной платы.",
-    "vacancyRichText": "<p>Вакансия компании \"MARPLA\"</p><p>МARketPLAce - Сервис анализа поисковых запросов на Wildberries.</p>Обязанности:<p>- Разработка и сопровождение парсера для сбора данных с маркетплейса Wildberries.</p>Требования:<p>- Положительный опыт работы в команде;<br />- Выходить во внерабочее время (при необходимости), для устранения критических и аварийных багов парсера;<br />- Высшее техническое образование;<br />- Опыт высоконагруженного парсинга:<br />многопоточный парсинг через прокси в несколько тысяч потоков;<br />от 100 млн запросов в сутки;<br />от 0.5 млрд записей результатов в сутки;<br />- Знание python, pandas;<br />- Понимание принципов работы поисковых систем, обязательных требований к сайтам;<br />- ООП, структуры данных, базовые алгоритмы и понимание их сложности, базовое знание HTML/CSS/JS, базовый Git.</p>Условия:<p>- Удаленный формат работы;<br />- Разнообразные интересные мотивирующие задачи;<br />- Своевременная выплата заработной платы.</p>",
-    "date_published": "2023.06.08 16:05:18",
-    "payment": {
-      "from": 100000,
-      "to": 0
-    }}
-    data2={
+    data = [{
     "platform": "HeadHunter",
-    "title": "Главный бухгалтер",
-    "company": "Безопасность",
-    "url": "https://hh.ru/vacancy/81551289",
+    "id": "81651060",
+    "title": "Заместитель главного бухгалтера",
+    "company": "Ищем работу вместе",
+    "url": "https://hh.ru/vacancy/81651060",
     "area": "Москва",
-    "address": "Москва, Щёлковское шоссе, 97",
-    "candidat": "Владение программами на уровне уверенного пользователя: MS Office (Word, Excel), 1С: <highlighttext>Бухгалтерия</highlighttext> 8.3, УНФ 8.3 , ЗУП, СБИС...",
-    "vacancyRichText": "Ведение бухгалтерского и налогового учета в полном объеме (книги покупок/продаж, учет материалов, затрат, основных средств (в т.ч. ",
-    "date_published": "2023.06.11 15:09:18",
+    "address": "",
+    "candidat": "Опыт работы в производственных компаниях. Знание 1С 8.3 <highlighttext>Бухгалтерия</highlighttext>, Управление торговлей, ЗУП, MS Office. Внимательность к деталям, аккуратность...",
+    "vacancyRichText": "Учет сырья, материалов. Ведение производственного учета: создание заказов на производство, списание сырья в производство, выпуск готовой продукции. Проверка производственных и...",
+    "date_published": "2023.06.08 23:41:07",
     "payment": {
-      "from": 0,
-      "to": 120000
-    }}
+      "from": 80000,
+      "to": 110000
+    }
+  },
+  {
+    "platform": "HeadHunter",
+    "id": "79718141",
+    "title": "Бухгалтер по расчету заработной платы",
+    "company": "LITOKOL",
+    "url": "https://hh.ru/vacancy/79718141",
+    "area": "Москва",
+    "address": "Москва, проезд Завода Серп и Молот, 6к1",
+    "candidat": "...<highlighttext>Бухгалтерия</highlighttext> 8.3, знаете бухгалтерский учет на участках: банк, расчеты с покупателями, авансовые отчеты. Являетесь уверенным пользователем 1С <highlighttext>Бухгалтерия</highlighttext>...",
+    "vacancyRichText": "Обеспечивать синхронизацию из 1С: ЗУП в 1С: <highlighttext>Бухгалтерия</highlighttext> и проводить операции по заработной плате и иным выплатам на...",
+    "date_published": "2023.05.31 12:20:18",
+    "payment": {
+      "from": 115000,
+      "to": 0
+    }}]
 
-    a = Vacancy(data)
 
-    b = Vacancy(data2)
-    print(a.__lt__(b))
-    print(Vacancy.all)
+
+    for b in data:
+        c = Vacancy(b)
+
+    print(c.all)
+
+    c.delete_vacancy("79718141")
+    print(c.all)
+
+
+
+
+
+
